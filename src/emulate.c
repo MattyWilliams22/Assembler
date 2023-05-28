93dc42b0fee6f2e5e0194a37f754aad6fa66fd62
kp1222 <kp1222@ic.ac.uk>
2023-05-28 23:11:01 +0000

    Data Processing Immediate - Arithmetic Operations added

--- src/emulate.c
+++ src/emulate.c
@@ -96,6 +96,64 @@ int decode(instruction instr) {
   }
 }
 
+// Data Processing Immediate
+
+void data_processing_immediate(instruction instr) {
+  // Extract bit 31
+  byte sf = (instr >> 31) & 0x1;
+  // Extract bits 30-29
+  byte opc = (instr >> 29) & 0x3;
+  // Extract bits 25-23
+  byte opi = (instr >> 23) & 0x7;
+  // Extract bits 22-5
+  byte operand = (instr >> 5) & 0x3ffff;
+  // Extract bits 4-0
+  byte rd = instr & 0x1f;
+
+  // Arithmetic operations - check opi is 010
+  if (opi == 0x2) {
+    // Extract bit 22
+    byte sh = (instr >> 22) & 0x1;
+    // Extract bits 21-10
+    byte imm12 = (instr >> 10) & 0xfff;
+    // Extract bits 9-5
+    byte rn = (instr >> 5) & 0x1f;
+
+    reg result;
+
+    // Calculate the result based on the opcode
+    switch (opc) {
+      case 0x0:  // ADD
+        result = STATE.registers[rn] + (sh ? (imm12 << 12) : imm12);
+        break;
+
+      case 0x1:  // ADDS
+        result = STATE.registers[rn] + (sh ? (imm12 << 12) : imm12);
+        STATE.pstate.n = (result >> (sf ? 63 : 31)) & 0x1;
+        STATE.pstate.z = (result == 0);
+        STATE.pstate.c = (result < STATE.registers[rn]);
+        STATE.pstate.v = (((STATE.registers[rn] ^ result) >> (sf ? 63 : 31)) & 0x1) && (((STATE.registers[rn] ^ (sh ? (imm12 << 12) : imm12)) >> (sf ? 63 : 31)) & 0x1);
+        break;
+
+      case 0x2:  // SUB
+        result = STATE.registers[rn] - (sh ? (imm12 << 12) : imm12);
+        break;
+
+      case 0x3:  // SUBS
+        result = STATE.registers[rn] - (sh ? (imm12 << 12) : imm12);
+        STATE.pstate.n = (result >> (sf ? 63 : 31)) & 0x1;
+        STATE.pstate.z = (result == 0);
+        STATE.pstate.c = (STATE.registers[rn] >= (sh ? (imm12 << 12) : imm12));
+        STATE.pstate.v = (((STATE.registers[rn] ^ result) >> (sf ? 63 : 31)) & 0x1) && (((STATE.registers[rn] ^ (sh ? (imm12 << 12) : imm12)) >> (sf ? 63 : 31)) & 0x1);
+        break;
+    }
+
+    // Store the result in the destination register
+    STATE.registers[rd] = result;
+  }
+}
+
+
 int main(int argc, char **argv) {
 
   if (argc != 2) {
