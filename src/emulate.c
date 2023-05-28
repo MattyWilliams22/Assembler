c8ea6bb59ea113ded2a4afe25a983949a01c6457
kp1222 <kp1222@ic.ac.uk>
2023-05-28 23:57:38 +0000

    Wide Move completed

--- src/emulate.c
+++ src/emulate.c
@@ -151,6 +151,30 @@ void data_processing_immediate(instruction instr) {
     // Store the result in the destination register
     STATE.registers[rd] = result;
   }
+
+  // Wide Move - check opi is 101
+  else if (opi == 0x5) {
+    // Extract bit 22-21
+    byte hw = (instr >> 21) & 0x3;
+    // Extract bits 20-5
+    byte imm16 = (instr >> 5) & 0xffff;
+
+    // Calculate the result based on the opcode
+    switch (opc) {
+      case 0x0:
+        // MOVZ
+        STATE.registers[rd] = (hw ? (imm16 << 16) : imm16);
+        break;
+      case 0x2:
+        // MOVN
+        STATE.registers[rd] = ~(hw ? (imm16 << 16) : imm16);
+        break;
+      case 0x3:
+        // MOVK
+
+    }
+  }
+
 }
 
 
