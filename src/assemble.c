224401cd5750c2fd2716743d4f9d5fd26560014a
kp1222 <kp1222@ic.ac.uk>
2023-05-30 00:52:28 +0000

    Created struct for symbol table represented as linked list, files can now be read and written

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,76 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+// Aliases for types
+
+typedef char* Key;
+typedef uint32_t Value;
+
+// Linked List Node
+typedef struct {
+  Key key;
+  Value value;
+  struct Node *next;
+} Node;
+
+// Linked List
+typedef struct {
+  Node *head;
+} Symbol_Table;
+
+// Adding to Symbol Table
+void add_to_table(Symbol_Table *table, Key key, Value value) {
+  Node *new_node = malloc(sizeof(Node));
+  new_node->key = key;
+  new_node->value = value;
+  new_node->next = NULL;
+
+  if (table->head == NULL) {
+    table->head = new_node;
+  } else {
+    Node *current = table->head;
+    while (current->next != NULL) {
+      current = current->next;
+    }
+    current->next = new_node;
+  }
+}
+
+// Removing from Symbol Table
+void remove_from_table(Symbol_Table *table, Key key) {
+  Node *current = table->head;
+  Node *previous = NULL;
+
+  while (current != NULL) {
+    if (strcmp(current->key, key) == 0) {
+      if (previous == NULL) {
+        table->head = current->next;
+      } else {
+        previous->next = current->next;
+      }
+      free(current);
+      return;
+    }
+    previous = current;
+    current = current->next;
+  }
+}
 
 int main(int argc, char **argv) {
+
+  FILE* input = fopen(argv[1], "rt");
+	if (input == NULL) {
+		perror("Could not open input file. Terminating.");
+		exit(EXIT_FAILURE);
+	}
+
+	FILE* output = fopen(argv[2], "wb+");
+	if (output == NULL) {
+		perror("Could not open output file. Terminating.");
+		exit(EXIT_FAILURE);
+	}
+
   return EXIT_SUCCESS;
 }
