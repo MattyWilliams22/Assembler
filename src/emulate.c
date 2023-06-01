2037cd942e7396755ff48d6b80922de15dc2fcf3
Dina Duong <dhd22@ic.ac.uk>
2023-06-01 17:32:31 +0100

    If program ran without output file specified, then memory state outpout to stdout

--- src/emulate.c
+++ src/emulate.c
@@ -772,23 +772,30 @@ void print_output(void) {
 
 int main(int argc, char **argv) {
 
+  if (argc == 2 || argc == 3) {
+
+    // Initialise the state of the memory
+    initialise_memory();
+
+    // Load binary file into memory
+    read_from_file(argv[1]);
+
+    // Process all of the read-in instructions
+    process_instructions();
+
+    if (argc == 2) {
+      // Output the state of the memory
+      print_output();
+    } else {
+      // Write the state of the memory to the file
+      write_to_file(argv[2]);
+    }
+
+    return EXIT_SUCCESS;
+  }
+
   if (argc != 3) {
     printf("Usage: ./emulate <input-filename> <output-filename>\n");
     return EXIT_FAILURE;
   }
-
-  // Initialise the state of the memory
-  initialise_memory();
-
-  // Load binary file into memory
-  read_from_file(argv[1]);
-
-  // Process all of the read-in instructions
-  process_instructions();
-
-  // Write the state of the memory to file
-  write_to_file(argv[2]);
-  print_output();
-
-  return EXIT_SUCCESS;
 }
